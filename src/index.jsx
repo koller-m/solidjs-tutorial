fac7fc2d5f42e68dd5e78c97ed8ca17dcf6ecf93
Mike Koller <mkoller@eml.cc>
2024-01-27 00:04:32 -0700

    Create basic functionality

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,34 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
+import { render } from "solid-js/web";
+import { createSignal } from "solid-js";
 
-import './index.css';
-import App from './App';
-
-const root = document.getElementById('root');
+import "./index.css";
+import App from "./App";
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
   throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
+    "Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?"
   );
 }
 
-render(() => <App />, root);
+function Reactivity() {
+  const [count, setCount] = createSignal(3);
+  const [multiplier, setMultiplier] = createSignal(3);
+  const product = () => count() * multiplier();
+
+  return (
+    <div>
+      <h1>
+        {count()} x {multiplier()} = {product()}
+      </h1>
+      <button onclick={() => setCount(count() + 1)}>increment</button>
+      <button onClick={() => setMultiplier(multiplier() + 1)}>
+        multiplier
+      </button>
+    </div>
+  );
+}
+
+render(App, document.getElementById("root"));
+
+render(Reactivity, document.getElementById("root"));
