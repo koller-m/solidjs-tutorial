01c90cb3d3e047971f82843e092b44e6cfcbe2d1
Mike Koller <mkoller@eml.cc>
2024-01-26 22:26:05 -0700

    Replace font color and title tag

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 function App() {
   return (
-    <p class="text-4xl text-green-700 text-center py-20">Hello Tailwind!</p>
+    <p class="text-4xl text-[#446B9E] text-center py-20">SolidJS Tutorial</p>
   );
 }
 
