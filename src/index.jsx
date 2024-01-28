d64ba01a658bda608a37547dd98b4ce2e8e0b890
Mike Koller <mkoller@eml.cc>
2024-01-27 19:22:18 -0700

    Add flip-clock animation

--- src/index.jsx
+++ src/index.jsx
@@ -14,23 +14,37 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 function Reactivity() {
   const [count, setCount] = createSignal(3);
   const [multiplier, setMultiplier] = createSignal(3);
+  const [buttonPressed, setButtonPressed] = createSignal(false);
   const product = () => count() * multiplier();
 
   return (
     <div class="flex flex-col items-center h-screen">
-      <h1 class="mb-8 text-3xl">
-        {count()} x {multiplier()} = {product()}
+      <h1
+        class={`mb-8 text-3xl flip-clock ${buttonPressed() ? "animate" : ""}`}
+        onAnimationEnd={() => setButtonPressed(false)}
+      >
+        <span class="digit">{count()}</span>
+        <span>&nbsp;x&nbsp;</span>
+        <span class="digit">{multiplier()}</span>
+        <span>&nbsp;=&nbsp;</span>
+        <span class="digit">{product()}</span>
       </h1>
       <div>
         <button
           class="bg-black text-white border-none rounded-sm mr-8 py-1 px-2"
-          onclick={() => setCount(count() + 1)}
+          onclick={() => {
+            setCount(count() + 1);
+            setButtonPressed(true);
+          }}
         >
           increment
         </button>
         <button
           class="bg-black text-white border-none rounded-sm py-1 px-2"
-          onClick={() => setMultiplier(multiplier() + 1)}
+          onClick={() => {
+            setMultiplier(multiplier() + 1);
+            setButtonPressed(true);
+          }}
         >
           multiplier
         </button>
